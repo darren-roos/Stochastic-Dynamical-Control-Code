97a67080e7cd345d11b9132c3b049bc19f5496d7
Darren Roos <darrencraigr@gmail.com>
2017-12-18 14:55:48 +0200

    Corrected indexing errors

--- py/openloop/Reactor_Linear_Models.py
+++ py/openloop/Reactor_Linear_Models.py
@@ -21,38 +21,38 @@ x1 = 0 # legend var
 x2 = 0 # legend var
 x3 = 0 # legend var
 for dd in range(nDD): # only loop through
-    initial_states = linsystems[k].op + (numpy.random.random([2])*2-1)*[0.01, 10]*dd
+    initial_states = linsystems[k].op + (numpy.random.random([2])*2-1)*[0.01, 10]*(dd+1)
 
     N = len(params.ts)
     xs = numpy.zeros([2, N])
     linxs = numpy.zeros([2, N])
     xs[:,0] = initial_states
     linxs[:,0] = initial_states - linsystems[k].b
-
     # Loop through the rest of time
     for t in range(1, N):
         xs[:, t] = params.cstr_model.run_reactor(xs[:, t-1], 0.0, params.h) # actual plant
-        linxs[:, t] = numpy.matmul(linsystems[k].A, linxs[:, t-1]) #+ linsystems[k].B*0.0
+        linxs[:, t] = numpy.matmul(linsystems[k].A, linxs[:, t-1]) + linsystems[k].B
+    
+    for i in range(len(linxs[0])):
+        linxs[:,i] += linsystems[k].b
 
-    for i in range(len(linxs)):
-        linxs[:,i] = linxs[:,i] + linsystems[k].b
-
+    
+        
     plt.subplot(nDD, 1, dd+1)
-    x1, = plt.plot(xs[0,:][:], xs[1,:][:], "k", linewidth=3)
-    x2, = plt.plot(linxs[0,:][:], linxs[1,:][:], "r--", linewidth=3)
-    plt.plot(xs[0,0], xs[1,0], "ko", markersize=10, markeredgewidth = 4)
-    plt.plot(xs[0,-1], xs[1,-1], "kx", markersize=10, markeredgewidth = 4)
-    plt.plot(linxs[0,0], linxs[1,0], "ro", markersize=10, markeredgewidth = 4)
-    plt.plot(linxs[0,-1], linxs[1,-1], "rx", markersize=10, markeredgewidth = 4)
+    x1, = plt.plot(xs[0], xs[1], "k", linewidth=3)
+    x2, = plt.plot(linxs[0], linxs[1], "r--", linewidth=3)
+    plt.plot(xs[0][0], xs[1][0], "ko", markersize=10, markeredgewidth = 4)
+    plt.plot(xs[0][-1], xs[1][-1], "kx", markersize=10, markeredgewidth = 4)
+    plt.plot(linxs[0][0], linxs[1][0], "ro", markersize=10, markeredgewidth = 4)
+    plt.plot(linxs[0][-1], linxs[1][-1], "rx", markersize=10, markeredgewidth = 4)
     plt.ylabel(r"T$_R$ [K]")
     plt.locator_params(nbins=6)
 
-
     ## Comment out as necessary!
-    if k==1:
+    if k==0:
         ss1 = [0.0097, 508.0562]
         x3, = plt.plot(ss1[0], ss1[1], "gx", markersize=10, markeredgewidth = 4)
-    elif k==2:
+    elif k==1:
         ss2 = [0.4893, 412.1302]
         x3, = plt.plot(ss2[0], ss2[1], "gx", markersize=10, markeredgewidth = 4)
     else:
