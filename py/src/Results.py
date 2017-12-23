af1e11c3a2569986c42de758e856a0be9c22cf94
Darren Roos <darrencraigr@gmail.com>
2017-12-23 09:32:43 +0200

    Added calc* functions

--- py/src/Results.py
+++ py/src/Results.py
@@ -12,7 +12,7 @@ def plotTracking(ts, xs, ys, fmeans, us, obs, setpoint):
     else:
         subplt = 3
         
-    mpl.rc("font", family="serif", serif="Computer Modern", size=12)
+    mpl.rc("font", family="serif", serif="Computer Modern", len=12)
     mpl.rc("text", usetex=True)
 
     skipmeas = int(len(ts)/80)
@@ -21,9 +21,9 @@ def plotTracking(ts, xs, ys, fmeans, us, obs, setpoint):
     plt.subplt.plot(subplt,1,1)
     x1, = plt.plot(ts, xs[0,:], "k", linewidth=3)
     if obs == 2: # plt.plot second measurement
-        y2, = plt.plot(ts[::skipmeas], ys[0][::skipmeas], "kx", markersize=5, markeredgewidth=1)
+        y2, = plt.plot(ts[::skipmeas], ys[0][::skipmeas], "kx", markerlen=5, markeredgewidth=1)
         
-    k1, = plt.plot(ts[::skipmean], fmeans[0][::skipmean], "bx", markersize=5, markeredgewidth = 2)
+    k1, = plt.plot(ts[::skipmean], fmeans[0][::skipmean], "bx", markerlen=5, markeredgewidth = 2)
     ksp = plt.plot(ts, setpoints, "g-", linewidth=3)
     plt.ylabel(r"C$_A$ [kmol.m$^{-3}$]")
     plt.locator_params(nbins=4)
@@ -33,11 +33,11 @@ def plotTracking(ts, xs, ys, fmeans, us, obs, setpoint):
     plt.subplt.plot(subplt,1,2)
     x2, = plt.plot(ts, xs[2,:], "k", linewidth=3)
     if obs == 1:
-        y2, = plt.plot(ts[::skipmeas], ys[::skipmeas], "kx", markersize=5, markeredgewidth=1)
+        y2, = plt.plot(ts[::skipmeas], ys[::skipmeas], "kx", markerlen=5, markeredgewidth=1)
     else:
-        y2, = plt.plot(ts[::skipmeas], ys[1][::skipmeas], "kx", markersize=5, markeredgewidth=1)
+        y2, = plt.plot(ts[::skipmeas], ys[1][::skipmeas], "kx", markerlen=5, markeredgewidth=1)
         
-    k2, = plt.plot(ts[::skipmean], fmeans[1][::skipmean], "bx", markersize=5, markeredgewidth = 2)
+    k2, = plt.plot(ts[::skipmean], fmeans[1][::skipmean], "bx", markerlen=5, markeredgewidth = 2)
     plt.ylabel(r"T$_R$ [K]")
     plt.locator_params(nbins=4)
     plt.legend([k2, y2],["Filtered mean", "Observations"], loc="best", ncol=2)
@@ -63,7 +63,7 @@ def plotTracking(ts, xs, ys, fmeans, us, obs):
     else:
         subplt = 3
     
-    mpl.rc("font", family="serif", serif="Computer Modern", size=12)
+    mpl.rc("font", family="serif", serif="Computer Modern", len=12)
     mpl.rc("text", usetex=True)
 
     skipmeas = int(len(ts)/80)
@@ -72,9 +72,9 @@ def plotTracking(ts, xs, ys, fmeans, us, obs):
     plt.subplt.plot(subplt,1,1)
     x1, = plt.plot(ts, xs[1,:], "k", linewidth=3)
     if obs == 1: # plt.plot second measurement
-        y2, = plt.plot(ts[::skipmeas], ys[0][::skipmeas], "kx", markersize=5, markeredgewidth=1)
+        y2, = plt.plot(ts[::skipmeas], ys[0][::skipmeas], "kx", markerlen=5, markeredgewidth=1)
     
-    k1, = plt.plot(ts[::skipmean], fmeans[0][::skipmean], "bx", markersize=5, markeredgewidth = 2)
+    k1, = plt.plot(ts[::skipmean], fmeans[0][::skipmean], "bx", markerlen=5, markeredgewidth = 2)
     plt.ylabel(r"C$_A$ [kmol.m$^{-3}$]")
     plt.locator_params(nbins=4)
     legend([x1],["Underlying model"], loc="best")
@@ -84,11 +84,11 @@ def plotTracking(ts, xs, ys, fmeans, us, obs):
     plt.subplt.plot(subplt,1,2)
     x2, = plt.plot(ts, xs[1,:], "k", linewidth=3)
     if obs == 0:
-        y2, = plt.plot(ts[::skipmeas], ys[::skipmeas], "kx", markersize=5, markeredgewidth=1)
+        y2, = plt.plot(ts[::skipmeas], ys[::skipmeas], "kx", markerlen=5, markeredgewidth=1)
     else:
-        y2, = plt.plot(ts[::skipmeas], ys[1][::skipmeas], "kx", markersize=5, markeredgewidth=1)
+        y2, = plt.plot(ts[::skipmeas], ys[1][::skipmeas], "kx", markerlen=5, markeredgewidth=1)
     
-    k2, = plt.plot(ts[::skipmean], fmeans[1][::skipmean], "bx", markersize=5, markeredgewidth = 2)
+    k2, = plt.plot(ts[::skipmean], fmeans[1][::skipmean], "bx", markerlen=5, markeredgewidth = 2)
     plt.ylabel(r"T$_R$ [K]")
     plt.locator_params(nbins=4)
     legend([k2, y2],["Filtered mean", "Observations"], loc="best")
@@ -106,21 +106,21 @@ def plotTracking(ts, xs, ys, fmeans, us, obs):
 
 
 def plotStateSpaceSwitch(linsystems, xs):
-    mpl.rc("font", family="serif", serif="Computer Modern", size=12)
+    mpl.rc("font", family="serif", serif="Computer Modern", len=12)
     mpl.rc("text", usetex=True)
     plt.figure() # Model and state space
     for k in range(len(linsystems)):
-        plt.plot(linsystems[k].op[0],linsystems[k].op[1],"kx",markersize=5, markeredgewidth=1)
+        plt.plot(linsystems[k].op[0],linsystems[k].op[1],"kx",markerlen=5, markeredgewidth=1)
         annotate(latexstring("M_",k),
             xy=[linsystems[k].op[0],linsystems[k].op[1]],
             xytext=[linsystems[k].op[0],linsystems[k].op[1]],
-            fontsize=12.0,
+            fontlen=12.0,
             ha="center",
             va="bottom")
         
     plt.plot(xs[0], xs[1], "k", linewidth=3)
-    plt.plot(xs[0][0], xs[1][0], "ko", markersize=10, markeredgewidth = 4)
-    plt.plot(xs[0][-1], xs[1][-1], "kx", markersize=10, markeredgewidth = 4)
+    plt.plot(xs[0][0], xs[1][0], "ko", markerlen=10, markeredgewidth = 4)
+    plt.plot(xs[0][-1], xs[1][-1], "kx", markerlen=10, markeredgewidth = 4)
     plt.xlim([-0.1, 1.1])
     plt.xlabel(r"C$_A$ [kmol.m$^{-3}$]")
     plt.ylabel(r"T$_R$ [K]")
@@ -130,7 +130,7 @@ def plotStateSpaceSwitch(linsystems, xs):
 def plotSwitchSelection(numSwitches, strack, ts, cbaron):
 
     plt.figure() # Model selection
-    mpl.rc("font", family="serif", serif="Computer Modern", size=12)
+    mpl.rc("font", family="serif", serif="Computer Modern", len=12)
     mpl.rc("text", usetex=True)
     axes = [None]*numSwitches
     im = 0
@@ -165,7 +165,7 @@ def plotSwitchSelection(numSwitches, strack, ts, cbaron):
 
 def plotEllipses(ts, xs, fmeans, fcovars, fname, legloc):
 
-    mpl.rc("font", family="serif", serif="Computer Modern", size=12)
+    mpl.rc("font", family="serif", serif="Computer Modern", len=12)
     mpl.rc("text", usetex=True)
     N = len(ts)
     skip = int(len(ts)/40)
@@ -177,9 +177,9 @@ def plotEllipses(ts, xs, fmeans, fcovars, fname, legloc):
         b1, = plt.fill(p1, p2, "b", edgecolor="none")
     
     x1, = plt.plot(xs[0,:], xs[1,:], "k",linewidth=3)
-    f1, = plt.plot(fmeans[0][::skip], fmeans[1][::skip], "mx", markersize=5, markeredgewidth = 2)
-    plt.plot(xs[0,0], xs[1,0], "ko", markersize=10, markeredgewidth = 4)
-    plt.plot(xs[0,-1], xs[1,-1], "kx", markersize=10, markeredgewidth = 4)
+    f1, = plt.plot(fmeans[0][::skip], fmeans[1][::skip], "mx", markerlen=5, markeredgewidth = 2)
+    plt.plot(xs[0,0], xs[1,0], "ko", markerlen=10, markeredgewidth = 4)
+    plt.plot(xs[0,-1], xs[1,-1], "kx", markerlen=10, markeredgewidth = 4)
     plt.ylabel(r"T$_R$ [K]")
     plt.xlabel(r"C$_A$ [kmol.m$^{-3}$]")
     plt.legend([x1,f1, b1],["Underlying model","Filtered mean", r"90$\%$ Confidence region"], loc=legloc)
@@ -188,7 +188,7 @@ def plotEllipses(ts, xs, fmeans, fcovars, fname, legloc):
 
 def plotEllipses(ts, xs, fmeans, fcovars, fname, line, sp, nf, sigma, pick, legloc):
 
-    mpl.rc("font", family="serif", serif="Computer Modern", size=12)
+    mpl.rc("font", family="serif", serif="Computer Modern", len=12)
     mpl.rc("text", usetex=True)
     N = len(ts)
     skip = int(len(ts)/40)
@@ -202,10 +202,10 @@ def plotEllipses(ts, xs, fmeans, fcovars, fname, line, sp, nf, sigma, pick, legl
         b1, = plt.fill(p1, p2, "b", edgecolor="none")
     
     x1, = plt.plot(xs[0], xs[1], "k",linewidth=3)
-    f1, = plt.plot(fmeans[0][::skip], fmeans[1][::skip], "mx", markersize=5, markeredgewidth = 2)
-    #plt.plot(xs[1, ::skip], xs[2, ::skip], "kx", markersize=5, markeredgewidth = 2)
-    plt.plot(xs[0,0], xs[1,0], "ko", markersize=10, markeredgewidth = 4)
-    plt.plot(xs[0,-1], xs[1,-1], "kx", markersize=10, markeredgewidth = 4)
+    f1, = plt.plot(fmeans[0][::skip], fmeans[1][::skip], "mx", markerlen=5, markeredgewidth = 2)
+    #plt.plot(xs[1, ::skip], xs[2, ::skip], "kx", markerlen=5, markeredgewidth = 2)
+    plt.plot(xs[0,0], xs[1,0], "ko", markerlen=10, markeredgewidth = 4)
+    plt.plot(xs[0,-1], xs[1,-1], "kx", markerlen=10, markeredgewidth = 4)
 
     # line = [b,c] => y + bx + c = 0
     # line => y = - bx - c
@@ -216,7 +216,7 @@ def plotEllipses(ts, xs, fmeans, fcovars, fname, line, sp, nf, sigma, pick, legl
     plt.ylim([min(xs[1,:]-10), max(xs[1, :]+10)])
     plt.plot(lxs, lys, "r-")
 
-    plt.plot(sp[0], sp[1], "gx",markersize=8, markeredgewidth = 4)
+    plt.plot(sp[0], sp[1], "gx",markerlen=8, markeredgewidth = 4)
 
     plt.ylabel(r"T$_R$ [K]")
     plt.xlabel(r"C$_A$ [kmol.m$^{-3}$]")
@@ -239,11 +239,11 @@ def plotEllipseComp(f1means, f1covars, f2means, f2covars, xs, ts, sigma=4.605):
     N = len(ts)
     skip = int(len(ts)/30)
     plt.figure()
-    mpl.rc("font", family="serif", serif="Computer Modern", size=12)
+    mpl.rc("font", family="serif", serif="Computer Modern", len=12)
     mpl.rc("text", usetex=True)
     x1, = plt.plot(xs[0], xs[1], "k",linewidth=3)
-    f1, = plt.plot(f1means[0][::skip], f1means[1][::skip], "yx", markersize=5, markeredgewidth = 2)
-    f2, = plt.plot(f2means[0][::skip], f2means[1][::skip], "gx", markersize=5, markeredgewidth = 2)
+    f1, = plt.plot(f1means[0][::skip], f1means[1][::skip], "yx", markerlen=5, markeredgewidth = 2)
+    f2, = plt.plot(f2means[0][::skip], f2means[1][::skip], "gx", markerlen=5, markeredgewidth = 2)
     b1 = 0.0
     b2 = 0.0
     for k in range(N):
@@ -256,8 +256,8 @@ def plotEllipseComp(f1means, f1covars, f2means, f2covars, xs, ts, sigma=4.605):
         b2, = fill(p3, p4, "b", edgecolor="none")
     
 
-    plt.plot(xs[0,0], xs[1,0], "ko", markersize=10, markeredgewidth = 4)
-    plt.plot(xs[0,-1], xs[1,-1], "kx", markersize=10, markeredgewidth = 4)
+    plt.plot(xs[0,0], xs[1,0], "ko", markerlen=10, markeredgewidth = 4)
+    plt.plot(xs[0,-1], xs[1,-1], "kx", markerlen=10, markeredgewidth = 4)
     plt.ylabel(r"T$_R$ [K]")
     plt.xlabel(r"C$_A$ [kmol.m$^{-3}$]")
     legend([x1,f1,f2, b1, b2],["Underlying model","Particle filter","Kalman filter", r"PF 90$\%$ Confidence region", r"KF 90$\%$ Confidence region"], loc="best")
@@ -270,13 +270,13 @@ def plotTrackingBreak(ts, xs, xsb, ys, fmeans, obs):
     tend = ts[end]
     skipm = int(len(ts)/80)
     plt.figure() # plt.plot filtered results
-    mpl.rc("font", family="serif", serif="Computer Modern", size=12)
+    mpl.rc("font", family="serif", serif="Computer Modern", len=12)
     mpl.rc("text", usetex=True)
     plt.subplt.plot(2,1,1)
     x1, = plt.plot(ts, xs[0,:], "k", linewidth=3)
     x1nf, = plt.plot(ts, xsb[0,:], "g--", linewidth=3)
     if obs == 2:
-        y2, = plt.plot(ts[::skipm], ys[::skipm], "kx", markersize=5, markeredgewidth=1)
+        y2, = plt.plot(ts[::skipm], ys[::skipm], "kx", markerlen=5, markeredgewidth=1)
     
     k1, = plt.plot(ts, fmeans[0,:], "r--", linewidth=3)
     plt.ylabel(r"C$_A$ [kmol.m$^{-3}$]")
@@ -287,9 +287,9 @@ def plotTrackingBreak(ts, xs, xsb, ys, fmeans, obs):
     x2, = plt.plot(ts, xs[2,:], "k", linewidth=3)
     x2nf, = plt.plot(ts, xsb[2,:], "g--", linewidth=3)
     if obs == 1:
-        y2, = plt.plot(ts[::skipm], ys[1][::skipm], "kx", markersize=5, markeredgewidth=1)
+        y2, = plt.plot(ts[::skipm], ys[1][::skipm], "kx", markerlen=5, markeredgewidth=1)
     else:
-        y2, = plt.plot(ts[::skipm], ys[::skipm], "kx", markersize=5, markeredgewidth=1)
+        y2, = plt.plot(ts[::skipm], ys[::skipm], "kx", markerlen=5, markeredgewidth=1)
     
     k2, = plt.plot(ts, fmeans[2,:], "r--", linewidth=3)
     plt.ylabel(r"T$_R$ [K]")
@@ -306,21 +306,21 @@ def plotTrackingTwoFilters(ts, xs, ys, f1means, f2means):
     skip = int(len(ts)/40)
     tend = ts[end]
     plt.figure() # plt.plot filtered results
-    mpl.rc("font", family="serif", serif="Computer Modern", size=12)
+    mpl.rc("font", family="serif", serif="Computer Modern", len=12)
     mpl.rc("text", usetex=True)
     plt.subplt.plot(2,1,1)
     x1, = plt.plot(ts, xs[0,:], "k", linewidth=3)
-    k1, = plt.plot(ts[::skip], f1means[0][::skip], "rx", markersize=5, markeredgewidth=2)
-    y2, = plt.plot(ts[::skipm], ys[0][::skipm], "kx", markersize=5, markeredgewidth=1)
-    k12, = plt.plot(ts[::skip], f2means[0][::skip], "bx", markersize=5, markeredgewidth=2)
+    k1, = plt.plot(ts[::skip], f1means[0][::skip], "rx", markerlen=5, markeredgewidth=2)
+    y2, = plt.plot(ts[::skipm], ys[0][::skipm], "kx", markerlen=5, markeredgewidth=1)
+    k12, = plt.plot(ts[::skip], f2means[0][::skip], "bx", markerlen=5, markeredgewidth=2)
     plt.ylabel(r"C$_A$ [kmol.m$^{-3}$]")
     plt.legend([x1, k1],["Underlying model","Particle filter"], loc="best", ncol=2)
     plt.xlim([0, tend])
     plt.subplt.plot(2,1,2)
     x2, = plt.plot(ts, xs[1,:], "k", linewidth=3)
-    y2, = plt.plot(ts[::skipm], ys[1][::skipm], "kx", markersize=5, markeredgewidth=1)
-    k2, = plt.plot(ts[::skip], f1means[1][::skip], "rx", markersize=5, markeredgewidth=2)
-    k22, = plt.plot(ts[::skip], f2means[1][::skip], "bx", markersize=5, markeredgewidth=2)
+    y2, = plt.plot(ts[::skipm], ys[1][::skipm], "kx", markerlen=5, markeredgewidth=1)
+    k2, = plt.plot(ts[::skip], f1means[1][::skip], "rx", markerlen=5, markeredgewidth=2)
+    k22, = plt.plot(ts[::skip], f2means[1][::skip], "bx", markerlen=5, markeredgewidth=2)
     plt.ylabel(r"T$_R$ [K]")
     plt.xlabel("Time [min]")
     legend([y2, k22],["Observations", "Kalman filter"], loc="best", ncol=2)
@@ -329,7 +329,7 @@ def plotTrackingTwoFilters(ts, xs, ys, f1means, f2means):
 
 
 def plotKLdiv(ts, kldiv, basediv, unidiv, logged):
-    mpl.rc("font", family="serif", serif="Computer Modern", size=12)
+    mpl.rc("font", family="serif", serif="Computer Modern", len=12)
     mpl.rc("text", usetex=True)
 
     plt.figure()
@@ -346,54 +346,52 @@ def plotKLdiv(ts, kldiv, basediv, unidiv, logged):
     plt.ylabel("Divergence [Nats]")
     legend([kl, gd, ud],["Approximation","Baseline", "Uniform"], loc="best")
     
-"""
-def calcError(x, y::Array{Float64, 2}):
 
-  r, N = size(x)
-  avediff1 = (1.0/N)*sum(abs((x[1, :].-y[1, :])./x[1,:]))*100.0
-  avediff2 = (1.0/N)*sum(abs((x[2, :].-y[2, :])./x[2,:]))*100.0
+def calcError(x, y):
+
+  r, N = len(x)
+  avediff1 = (1/N)*sum(abs((x[0]-y[0])/x[0]))*100
+  avediff2 = (1/N)*sum(abs((x[1]-y[1])/x[1]))*100
 
   print("Average Concentration Error: ", round(avediff1, 4),  "%")
   print("Average Temperature Error: ", round(avediff2, 4), "%")
   return avediff1, avediff2
-end
 
-def calcError(x, y::Float64):
+def calcError(x, y):
 
-  r, N = size(x)
-  avediff1 = (1.0/N)*sum(abs((x[1, :] - y)./y))*100.0
+    r, N = len(x)
+    avediff1 = (1/N)*sum(abs((x[0] - y)/y))*100
 
-  print("Average Concentration Error: ", round(avediff1, 4),  "%")
-  return avediff1
-end
+    print("Average Concentration Error: ", round(avediff1, 4),  "%")
+    return avediff1
 
-def calcError2(x, y::Float64):
 
-  r, N = size(x)
-  avediff1 = (1.0/N)*sum(abs((x[1, :] - y)./y))*100.0
+def calcError2(x, y):
 
-  return avediff1
-end
+    r, N = len(x)
+    avediff1 = (1/N)*sum(abs((x[0] - y)/y))*100
 
-def calcError3(x, y::Float64):
+    return avediff1
 
-  N = len(x)
-  avediff1 = (1.0/N)*sum(abs((x .- y)./y))*100.0
+def calcError3(x, y):
 
-  return avediff1
-end
+    N = len(x)
+    avediff1 = (1/N)*sum(abs((x - y)/y))*100
+
+    return avediff1
+
 
 def calcEnergy(us, uss, h):
-  N = len(us)
-  avecost = (1./(60.0*N))*sum(abs(us-uss))
-  print("Average Input (kW): ", avecost)
-  return avecost
-end
+    N = len(us)
+    avecost = (1/(60*N))*sum(abs(us-uss))
+    print("Average Input (kW): ", avecost)
+    return avecost
 
+"""
 def checkConstraint(ts, xs, line):
   # line = [b,c] => y + bx + c = 0
   # line => y = - bx - c
-  r, N = size(xs)
+  r, N = len(xs)
   conmargin = zeros(N)
   minneg = Inf
   minpos = Inf
@@ -417,7 +415,7 @@ def checkConstraint(ts, xs, line):
   print("Minimum Negative Clearance: ", minneg)
 
   plt.figure()
-  mpl.rc("font", family="serif", size=12)
+  mpl.rc("font", family="serif", len=12)
   mpl.rc("text", usetex=True)
 
   plt.plot(ts, zeros(N), "r", linewidth=1)
@@ -430,7 +428,7 @@ def getMCRes!(xs, sigmas, line, mcdistmat, counter, h):
   # line = [b,c] => y + bx + c = 0
   # line => y = - bx - c
   d = [line[1], 1.0]
-  r, N = size(xs)
+  r, N = len(xs)
   negdist = 0.0
   timeviolated = 0
   for k=1:N
